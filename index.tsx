4470f1b4cd64cf323328e03cffc9b27fb698d6f9
catalinmiron <mironcatalin@gmail.com>
2018-12-14 16:25:34 +0100

    Updates

--- index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './src/index'
-
-ReactDOM.render(
-  <Root name="Catalin" />,
-  document.getElementById("root")
-);
--- src/index.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-
-import Root, {Greeting} from "./index";
-
-it("renders the heading", () => {
-    const result = shallow(<Root name="Catalin" />).contains(<Greeting>Zalut, Catalin!</Greeting>);
-    expect(result).toBeTruthy();
-});
